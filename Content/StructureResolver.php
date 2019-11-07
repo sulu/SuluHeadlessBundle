9c4e4800dbb762a67e1797c37894091891ac9786
Niklas Natter <niklas@sulu.io>
2019-11-07 13:23:26 +0000

    Remove parentId from StructureResolver

--- Content/StructureResolver.php
+++ Content/StructureResolver.php
@@ -37,15 +37,11 @@ class StructureResolver implements StructureResolverInterface
         /** @var BasePageDocument $document */
         $document = $structure->getDocument();
 
-        /** @var StructureBridge|null $parent */
-        $parent = $structure->getParent();
-
         /** @var \DateTimeInterface $authored */
         $authored = $document->getAuthored();
 
         $data = [
             'id' => $structure->getUuid(),
-            'parentId' => $parent ? $parent->getUuid() : null,
             'type' => 'page',
             'template' => $document->getStructureType(),
             'content' => [],
