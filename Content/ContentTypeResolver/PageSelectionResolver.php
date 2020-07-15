73977e4f923d8ece66e90573e59c627906d848ba
Niklas Natter <niklas@sulu.io>
2020-07-15 15:00:09 +0200

    Use ContentMapper service for loading pages in PageSelectionResolver

--- Content/ContentTypeResolver/PageSelectionResolver.php
+++ Content/ContentTypeResolver/PageSelectionResolver.php
@@ -14,10 +14,11 @@ declare(strict_types=1);
 namespace Sulu\Bundle\HeadlessBundle\Content\ContentTypeResolver;
 
 use Sulu\Bundle\HeadlessBundle\Content\ContentView;
-use Sulu\Bundle\HeadlessBundle\Content\Serializer\PageSerializerInterface;
-use Sulu\Bundle\PageBundle\Content\PageSelectionContainer;
+use Sulu\Bundle\HeadlessBundle\Content\StructureResolverInterface;
 use Sulu\Component\Content\Compat\PropertyInterface;
 use Sulu\Component\Content\Compat\PropertyParameter;
+use Sulu\Component\Content\Mapper\ContentMapperInterface;
+use Sulu\Component\Content\Query\ContentQueryBuilderInterface;
 
 class PageSelectionResolver implements ContentTypeResolverInterface
 {
@@ -27,52 +28,71 @@ class PageSelectionResolver implements ContentTypeResolverInterface
     }
 
     /**
-     * @var PageSerializerInterface
+     * @var StructureResolverInterface
      */
-    private $pageSerializer;
+    private $structureResolver;
 
     /**
-     * @var PageSelectionContainerFactory
+     * @var ContentQueryBuilderInterface
      */
-    private $pageSelectionContainerFactory;
+    private $contentQueryBuilder;
+
+    /**
+     * @var ContentMapperInterface
+     */
+    private $contentMapper;
+
+    /**
+     * @var bool
+     */
+    private $showDrafts;
 
     public function __construct(
-        PageSerializerInterface $pageSerializer,
-        PageSelectionContainerFactory $pageSelectionContainerFactory
+        StructureResolverInterface $structureResolver,
+        ContentQueryBuilderInterface $contentQueryBuilder,
+        ContentMapperInterface $contentMapper,
+        bool $showDrafts
     ) {
-        $this->pageSerializer = $pageSerializer;
-        $this->pageSelectionContainerFactory = $pageSelectionContainerFactory;
+        $this->structureResolver = $structureResolver;
+        $this->contentQueryBuilder = $contentQueryBuilder;
+        $this->contentMapper = $contentMapper;
+        $this->showDrafts = $showDrafts;
     }
 
     public function resolve($data, PropertyInterface $property, string $locale, array $attributes = []): ContentView
     {
-        if (null === $data) {
+        if (empty($data)) {
             return new ContentView([]);
         }
 
         /** @var PropertyParameter[] $params */
-        $params = array_merge(
-            ['properties' => new PropertyParameter('properties', [], 'collection')],
-            $property->getParams()
+        $params = $property->getParams();
+        /** @var PropertyParameter[] $propertiesParamValue */
+        $propertiesParamValue = isset($params['properties']) ? $params['properties']->getValue() : [];
+
+        $this->contentQueryBuilder->init([
+            'ids' => $data,
+            'properties' => $propertiesParamValue,
+            'published' => !$this->showDrafts,
+        ]);
+        list($pagesQuery) = $this->contentQueryBuilder->build($property->getStructure()->getWebspaceKey(), [$locale]);
+
+        $pageStructures = $this->contentMapper->loadBySql2(
+            $pagesQuery,
+            $locale,
+            $property->getStructure()->getWebspaceKey()
         );
 
-        // the PageSelectionContainer resolves the data defined in the $params using the default content types
-        // for example, this means that the result contains an array of media entities instead of an array of ids
-        // TODO: find a solution that returns the unresolved data which can be passed to the resolvers of this bundle
-        /** @var mixed[] $pagesData */
-        $pagesData = $this->pageSelectionContainerFactory->createContainer(
-            $data,
-            $params,
-            $attributes['webspaceKey'] ?? null,
-            $locale
-        )->getData();
-
-        /** @var PropertyParameter[] $propertyParameters */
-        $propertyParameters = $params['properties']->getValue();
+        $propertyMap = [];
+        foreach ($propertiesParamValue as $propertiesParamEntry) {
+            /** @var string $propertyValue */
+            $propertyValue = $propertiesParamEntry->getValue();
+            $propertyMap[$propertiesParamEntry->getName()] = $propertyValue;
+        }
 
         $pages = [];
-        foreach ($pagesData as $pageData) {
-            $pages[] = $this->pageSerializer->serialize($pageData, $propertyParameters);
+        foreach ($pageStructures as $pageStructure) {
+            $pages[] = $this->structureResolver->resolveProperties($pageStructure, $propertyMap, $locale);
         }
 
         return new ContentView($pages, $data);
