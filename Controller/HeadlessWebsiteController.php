f06503e29f113869270a217e48526ee633b2bf8c
nnatter <niklas@sulu.io>
2020-11-26 16:11:58 +0100

    Adjust HeadlessWebsiteController to be compatible with DefaultController (#59)

--- Controller/HeadlessWebsiteController.php
+++ Controller/HeadlessWebsiteController.php
@@ -16,17 +16,13 @@ namespace Sulu\Bundle\HeadlessBundle\Controller;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Sulu\Bundle\HeadlessBundle\Content\StructureResolverInterface;
-use Sulu\Bundle\HttpCacheBundle\CacheLifetime\CacheLifetimeEnhancer;
-use Sulu\Bundle\HttpCacheBundle\CacheLifetime\CacheLifetimeEnhancerInterface;
-use Sulu\Bundle\PreviewBundle\Preview\Preview;
+use Sulu\Bundle\WebsiteBundle\Controller\WebsiteController;
 use Sulu\Component\Content\Compat\PageInterface;
 use Sulu\Component\Content\Compat\StructureInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class HeadlessWebsiteController extends AbstractController
+class HeadlessWebsiteController extends WebsiteController
 {
     /**
      * We cannot set the typehint of the $structure parameter to PageInterface because the ArticleBundle does not
@@ -40,52 +36,21 @@ class HeadlessWebsiteController extends AbstractController
         bool $preview = false,
         bool $partial = false
     ): Response {
-        /** @var string $requestFormat */
-        $requestFormat = $request->getRequestFormat();
-        $data = $this->resolveStructure($structure);
-        $json = $this->serializeData($data);
+        $headlessData = $this->resolveStructure($structure);
 
-        $response = null;
         if ('json' !== $request->getRequestFormat()) {
-            $data['jsonData'] = $json;
-            $response = $this->renderTemplateResponse($structure, $requestFormat, $preview, $data);
-        } else {
-            $response = new Response($json);
-            $response->headers->set('Content-Type', 'application/json');
+            return $this->renderStructure($structure, ['headless' => $headlessData], $preview, $partial);
         }
 
-        $cacheLifetimeEnhancer = $this->getCacheLifetimeEnhancer();
+        $response = new Response($this->serializeData($headlessData));
+        $response->headers->set('Content-Type', 'application/json');
+
+        $cacheLifetimeEnhancer = $this->getCacheTimeLifeEnhancer();
         if (!$preview && $cacheLifetimeEnhancer) {
             $cacheLifetimeEnhancer->enhance($response, $structure);
         }
 
         return $response;
-    }
-
-    /**
-     * @param PageInterface $structure
-     * @param mixed[] $parameters
-     */
-    private function renderTemplateResponse(
-        StructureInterface $structure,
-        string $requestFormat,
-        bool $preview,
-        array $parameters
-    ): Response {
-        $viewTemplate = $structure->getView() . '.' . $requestFormat . '.twig';
-
-        if (!$this->get('twig')->getLoader()->exists($viewTemplate)) {
-            throw new HttpException(406, sprintf('Page does not exist in "%s" format.', $requestFormat));
-        }
-
-        if ($preview) {
-            $parameters['previewParentTemplate'] = $viewTemplate;
-            $parameters['previewContentReplacer'] = Preview::CONTENT_REPLACER;
-
-            return $this->render('@SuluWebsite/Preview/preview.html.twig', $parameters);
-        }
-
-        return $this->render($viewTemplate, $parameters);
     }
 
     /**
@@ -119,20 +84,7 @@ class HeadlessWebsiteController extends AbstractController
         $subscribedServices = parent::getSubscribedServices();
         $subscribedServices['sulu_headless.structure_resolver'] = StructureResolverInterface::class;
         $subscribedServices['jms_serializer'] = SerializerInterface::class;
-        $subscribedServices['sulu_http_cache.cache_lifetime.enhancer'] = '?' . CacheLifetimeEnhancerInterface::class;
 
         return $subscribedServices;
-    }
-
-    protected function getCacheLifetimeEnhancer(): ?CacheLifetimeEnhancer
-    {
-        if (!$this->has('sulu_http_cache.cache_lifetime.enhancer')) {
-            return null;
-        }
-
-        /** @var CacheLifetimeEnhancer $cacheLifetimeEnhancer */
-        $cacheLifetimeEnhancer = $this->get('sulu_http_cache.cache_lifetime.enhancer');
-
-        return $cacheLifetimeEnhancer;
     }
 }
