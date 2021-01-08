a78ffcc295d60300841076bdaf9316d9e907b785
nnatter <niklas@sulu.io>
2021-01-08 17:02:29 +0100

    Expand composer lint script and add php-cs-fixer as dev-dependency (#68)

--- Tests/Application/config/bootstrap.php
+++ Tests/Application/config/bootstrap.php
@@ -13,7 +13,8 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-$file = __DIR__ . '/../vendor/autoload.php';
+$file = __DIR__ . '/../../../vendor/autoload.php';
+
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
@@ -28,38 +29,9 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
-    $path = dirname(__DIR__) . '/Tests/Application/.env';
+    $path = dirname(__DIR__) . '/.env';
     $dotenv = new Dotenv();
-
-    // load all the .env files
-    if (method_exists($dotenv, 'loadEnv')) {
-        $dotenv->loadEnv($path);
-    } else {
-        // fallback code in case your Dotenv component is not 4.2 or higher (when loadEnv() was added)
-
-        if (file_exists($path) || !file_exists($p = "$path.dist")) {
-            $dotenv->load($path);
-        } else {
-            $dotenv->load($p);
-        }
-
-        if (null === $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) {
-            $dotenv->populate(['APP_ENV' => $env = 'dev']);
-        }
-
-        if ('test' !== $env && file_exists($p = "$path.local")) {
-            $dotenv->load($p);
-            $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env;
-        }
-
-        if (file_exists($p = "$path.$env")) {
-            $dotenv->load($p);
-        }
-
-        if (file_exists($p = "$path.$env.local")) {
-            $dotenv->load($p);
-        }
-    }
+    $dotenv->loadEnv($path);
 }
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
