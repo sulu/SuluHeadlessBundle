7dd0a84b0d7aa51b057a23d139fcbead9bf050f7
Luca Rath-Heel <luca@sulu.io>
2021-04-15 18:14:42 +0200

    Fix HeadlessBundleKernelBrowser

--- Tests/Application/Testing/HeadlessBundleKernelBrowser.php
+++ Tests/Application/Testing/HeadlessBundleKernelBrowser.php
@@ -40,6 +40,42 @@ class HeadlessBundleKernelBrowser extends KernelBrowser
         array $server = [],
         bool $changeHistory = true
     ): Crawler {
-        return $this->request($method, $uri, $parameters, [], $server, null, $changeHistory);
+        // Based on https://github.com/symfony/symfony/blob/v5.2.0/src/Symfony/Component/HttpFoundation/Request.php#L388-L404
+        // the logic in symfony/http-foundation we convert parameters to json content.
+        switch (\strtoupper($method)) {
+            case 'POST':
+            case 'PUT':
+            case 'DELETE':
+            case 'PATCH':
+                $content = \json_encode($parameters, \defined('JSON_THROW_ON_ERROR') ? \JSON_THROW_ON_ERROR : 0);
+                $query = [];
+                break;
+            default:
+                $content = null;
+                $query = $parameters;
+                break;
+        }
+
+        $serverContentType = $this->getServerParameter('CONTENT_TYPE', null);
+        $serverHttpAccept = $this->getServerParameter('HTTP_ACCEPT', null);
+
+        $this->setServerParameter('CONTENT_TYPE', 'application/json');
+        $this->setServerParameter('HTTP_ACCEPT', 'application/json');
+
+        try {
+            return $this->request($method, $uri, $query, [], $server, $content, $changeHistory);
+        } finally {
+            if (null === $serverContentType) {
+                unset($this->server['CONTENT_TYPE']);
+            } else {
+                $this->setServerParameter('CONTENT_TYPE', $serverContentType);
+            }
+
+            if (null === $serverHttpAccept) {
+                unset($this->server['HTTP_ACCEPT']);
+            } else {
+                $this->setServerParameter('HTTP_ACCEPT', $serverHttpAccept);
+            }
+        }
     }
 }
